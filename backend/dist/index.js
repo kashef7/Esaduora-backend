40c58547286bd5f34675d1d0b9d98d03c9c5d3d3
kashef7 <11410120230477@stud.cu.edu.eg>
2025-09-09 00:00:23 +0300

    Added index and server files to connect to server and db

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,4 +1,13 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-console.log("Hello World!");
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+export const app = express();
+app.use(express.json({
+    limit: '10kb'
+}));
+app.use(cookieParser());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}));
 //# sourceMappingURL=index.js.map
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,17 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
-console.log("Hello World");
+export const app = express();
 
+app.use(express.json({
+  limit:'10kb'
+}))
+
+app.use(cookieParser());
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
 
